172d0a380a8857a3176f0f1034915f9fbf55a5cd
canseldegdas <48032173+canseldegdas@users.noreply.github.com>
2021-12-20 19:10:19 +0300

    Add files via upload

---
+++ coins/coins/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coins
+//
+//  Created by Cansel Değdaş on 20.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coins/coins/coinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  coinsApp.swift
+//  coins
+//
+//  Created by Cansel Değdaş on 20.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct coinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
